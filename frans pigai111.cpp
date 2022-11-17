88eeeffdd0c0288562614261eeed83714d0de8ef
pigaifery <118467177+pigaifery@users.noreply.github.com>
2022-11-18 00:53:45 +0700

    Add files via upload

---
+++ frans pigai111.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Frans Pigai\nX - Multimedia\n" << endl;
+    int angka[5];
+
+    angka[0] = 20;
+    angka[1] = 15;
+    angka[2] = 4;
+    angka[3] = 22;
+    angka[4] = 90;
+
+    cout << "Array ke-3 bernilai = " << angka[2] <<endl;
+    cout << "Array ke-5 bernilai = " << angka[4] <<endl;
+}
---
+++ frans pigai222.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    int ArrayA[a][b];
+
+    cout<<"Input 1: ";cin>>ArrayA[0][0];
+    cout<<"Input 2: ";cin>>ArrayA[1][0];
+
+    cout<<"Row 1 column 1: "<<ArrayA[0][0]<<endl;
+    cout<<"Row 2 column 1: "<<ArrayA[1][0]<<endl;
+
+system("pause");
+return 0;
+}
